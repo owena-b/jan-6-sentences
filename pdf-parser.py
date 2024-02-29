d139e8ec8a731ea7fd45a8967a350802e0642311
owen a-b <owen@owenab.com>
2024-02-29 13:47:14 -0500

    Generally works as one pdf, with some bugs where source cells are blank

--- pdf-parser.py
+++ pdf-parser.py
@@ -1,18 +1,33 @@
 import camelot
-import pandas as pd
+import csv
 
 src = 'table.pdf'
 
-tables = camelot.read_pdf(src, strip_text='\n', pages='1')
+tables = camelot.read_pdf(src, strip_text='\n', pages='1-10')
 
 headers = ['Defendant First Name', 'Defendant Last Name', 'Case Number', 'Offense', 'Government Recommendation',
            'Sentence Imposed']
 
-#df = pd.DataFrame(columns=headers)
-df = tables.df
+bigList = []
 
-#for table in tables:
-#    temp = table.df.copy()
-#    df = pd.concat([df, temp])
+for table in tables:
+    temp = table.df.values.tolist()
+    bigList.append(temp)
 
-df.to_csv('sentences.csv')
+print(bigList)
+
+with open('sentences.csv', 'w', newline='', encoding='utf-8') as outfile:
+    writer = csv.writer(outfile)
+    writer.writerow(headers)
+
+    for table in bigList:
+        for cell in table[1:]:
+            if ', ' in cell[0]:
+                split_str = cell[0].split(', ')
+                cell[0] = split_str[1]
+                cell.insert(1, split_str[0])
+            else:
+                cell.insert(1, '')
+        writer.writerows(table[1:])
+
+    print("Wrote sentences.csv")
